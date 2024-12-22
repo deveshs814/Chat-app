dcc96d32523f871790debf531bf99a9b0d5b8753
Devesh Shukla <shukladevesh9814@gmail.com>
2024-12-22 14:32:01 +0530

    Setup the database connection and created the user schema.

---
+++ Chat/backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import authRoutes from './routes/authRoute.js'
+import dotenv from 'dotenv'
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+dotenv.config()
+
+const port = process.env.PORT
+
+app.use('/api/auth',authRoutes)
+
+app.listen(port,() =>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+    connectDB();
+})
