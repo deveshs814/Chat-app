30bf48cf1e3f212b29173a3e1bb1e78ac24fab16
Devesh Shukla <shukladevesh9814@gmail.com>
2025-01-05 10:53:33 +0530

    Created the message Controller.

--- Chat/backend/src/index.js
+++ Chat/backend/src/index.js
@@ -3,7 +3,7 @@ import authRoutes from './routes/authRoute.js'
 import dotenv from 'dotenv'
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser'
-
+import messageRoutes from './routes/messageRoute.js'
 const app = express();
 dotenv.config()
 
@@ -12,6 +12,7 @@ const port = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth',authRoutes)
+app.use('/api/message',messageRoutes)
 
 app.listen(port,() =>{
     console.log(`Server is running on port ${process.env.PORT}`)
