ff7b28c644d6bdcc7827b1f8fcee4fbae3628144
Devesh Shukla <shukladevesh9814@gmail.com>
2025-01-06 23:11:16 +0530

    Created the Signup and login pages and implemented daisy ui.

--- Chat/backend/src/index.js
+++ Chat/backend/src/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/messageRoute.js'
+import cors from 'cors';
+
 const app = express();
 dotenv.config()
 
@@ -11,6 +13,12 @@ const port = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
+
+
 app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
