03e4b5a736b1220023e94f2107dd3813d86a47f1
Devesh Shukla <shukladevesh9814@gmail.com>
2025-01-10 22:05:42 +0530

    Created the Homepage side container

--- Chat/backend/src/index.js
+++ Chat/backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 
 app.use('/api/auth',authRoutes)
-app.use('/api/message',messageRoutes)
+app.use('/api/messages',messageRoutes)
 
 app.listen(port,() =>{
     console.log(`Server is running on port ${process.env.PORT}`)
