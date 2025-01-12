31de171d8eba6e6c6bbf2d8550768fba4810f099
Devesh Shukla <shukladevesh9814@gmail.com>
2025-01-12 21:35:12 +0530

    Changes done in chat container

--- Chat/frontend/src/Components/ChatContainer.jsx
+++ Chat/frontend/src/Components/ChatContainer.jsx
@@ -2,8 +2,6 @@ import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput";
-import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
