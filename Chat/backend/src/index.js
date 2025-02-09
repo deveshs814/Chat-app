30c0d423f5eb4fbe8befdb25192d37d1c0fca817
Devesh Shukla <shukladevesh9814@gmail.com>
2025-02-09 18:20:35 +0530

    Project completed

--- Chat/backend/src/index.js
+++ Chat/backend/src/index.js
@@ -6,22 +6,35 @@ import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/messageRoute.js'
 import cors from 'cors';
 import { app, server } from "./lib/socket.js";
-
+import bodyParser from "body-parser";
 dotenv.config()
+import path from "path";
 
 const port = process.env.PORT
+const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin:'http://localhost:5173',
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true
 }))
 
+app.use(bodyParser.json({ limit: "50mb" })); // JSON limit
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // URL-encoded limit
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+}
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
 server.listen(port,() =>{
     console.log(`Server is running on port ${process.env.PORT}`)
     connectDB();
--- Chat/frontend/src/lib/axios.js
+++ Chat/frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001"; 
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,  // Ensure cookies are sent if using authentication
 });
